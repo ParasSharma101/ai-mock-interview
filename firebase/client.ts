e94be258d3e6a7d739f5e260d45d8e8b050ebaa9
PARAS SHARMA <145317890+ParasSharma101@users.noreply.github.com>
2025-04-13 15:36:59 +0530

    refactor: update Firebase client configuration to use environment variables and improve initialization logic

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+ import { initializeApp, getApp, getApps } from "firebase/app";
+ import { getAuth } from "firebase/auth";
+ import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC_lCL_wudRPARWqF84kWAxZw3vNrfHrY8",
-  authDomain: "wiseprep-5023c.firebaseapp.com",
-  projectId: "wiseprep-5023c",
-  storageBucket: "wiseprep-5023c.firebasestorage.app",
-  messagingSenderId: "1034532359140",
-  appId: "1:1034532359140:web:c3535dbadac12ddf0b55f1",
-  measurementId: "G-C9H8JME0CX"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
